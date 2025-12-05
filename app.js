ec3767d95c647660c98c46bab4e2a6215f1956fa
heshan <heshansandakelum2018@gmail.com>
2025-12-06 05:09:28 +0530

    complete js on register button and form submit

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-console.log("hello"
+const regBtnClick = () => {
+    window.location.href="form.html"
+}
 
-)
+//get data
+document.addEventListener("DOMContentLoaded",function(){
+    const m =document.getElementById("formData");
+    m.addEventListener("submit",function(e){
+        e.preventDefault();
+        const formData =new FormData(m)
+        const data = Object.fromEntries(formData.entries())
+        console.log(data);
+        m.reset();
+        })
+})
